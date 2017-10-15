fdb987a6b4784e0f4c741721ade0a8e3f2ee125b
Ravin <milknaiti@hotmail.com>
2017-10-15 13:19:36 -0700

    There are bugs in process add pig and chic to stove.

--- Actor.py
+++ Actor.py
@@ -1,7 +1,9 @@
 import arcade
-import time
+import random
 from pyglet.window import mouse
 roasting = 5.0
+
+
 class Model:
     def __init__(self, world, x, y):
         self.world = world
@@ -9,6 +11,13 @@ class Model:
         self.y = y
     def hit(self, other, hit_size):
         return (abs(self.x - other.x) <= hit_size) and (abs(self.y - other.y) <= hit_size)
+
+class Collectable(arcade.Sprite):
+    """ This class represents something the player collects. """
+    def __init__(self, filename, scale):
+        super().__init__(filename, scale)
+        # Flip this once the coing has been collected.
+        self.changed = False
 
 class Player(Model):
 
@@ -19,160 +28,175 @@ class Player(Model):
         self.x = x
         self.y = y
 
-
-    def on_mouse_drag(self,x, y, dx, dy, buttons, modifiers):
-        if buttons and mouse.LEFT:
-            self.x = x
-            self.y = y
-
     def update(self, delta):
-        if self.x<0:
-            self.x += 10
-        elif self.x>self.world.width:
-            self.x -=10
-        elif self.y<0:
-            self.y +=10
-        elif self.y>self.world.height:
-            self.y -=10
+       pass
 
+class Pig(arcade.Sprite):
+    def __init__(self, filename, scale):
+        super().__init__(filename, scale)
+        # Flip this once the coing has been collected.
+        self.changed = False
+    def update(self):
+        pass
+        
+class Beef(arcade.Sprite):
+    def __init__(self, filename, scale):
+        super().__init__(filename, scale)
+        # Flip this once the coing has been collected.
+        self.changed = False
+    def update(self):
+        pass
 
-class Beef(Model):
+class Chicken(arcade.Sprite):
+    def __init__(self, filename, scale):
+        super().__init__(filename, scale)
+        # Flip this once the coing has been collected.
+        self.changed = False
+    def update(self):
+        pass
 
-    def __init__(self, world, x,y):
-        super().__init__(world, x, y)
-
-    def update(self, delta):
-        if self.x>28 and self.x<460 and self.y>30 and self.y<100:
-            pass#print('Beef is on stove.')
-            
-class Pig(Model):
-
-    def __init__(self, world, x,y):
-        super().__init__(world, x, y)
-
-    def update(self, delta):
-        if self.x>28 and self.x<460 and self.y>30 and self.y<100:
-            pass#print('Pig is on stove.')
-class Chic(Model):
-
-    def __init__(self, world, x,y):
-        super().__init__(world, x, y)
-
-    def update(self, delta):
-        if self.x>28 and self.x<460 and self.y>30 and self.y<100:
-            pass#print('Chicken is on stove.')
-class BeefWell(Model):
+class Stove1(Model):
 
     def __init__(self, world, x,y):
         super().__init__(world, x, y)
 
     def update(self, delta):
         pass
-class PigWell(Model):
+class Stove2(Model):
 
     def __init__(self, world, x,y):
         super().__init__(world, x, y)
 
     def update(self, delta):
-        if self.x<0:
-            self.x = 0
-class ChicWell(Model):
+        pass
+class Stove3(Model):
 
     def __init__(self, world, x,y):
         super().__init__(world, x, y)
 
     def update(self, delta):
-        if self.x<0:
-            self.x = 0
-
-class Tau(Model):
+        pass
+class Stove4(Model):
 
     def __init__(self, world, x,y):
         super().__init__(world, x, y)
 
     def update(self, delta):
-        if self.x<0:
-            self.x = 0
-class Plate(Model):
+        pass
+class Stove5(Model):
 
     def __init__(self, world, x,y):
         super().__init__(world, x, y)
 
     def update(self, delta):
-        if self.x<0:
-            self.x = 0
+        pass
+class Stove6(Model):
+
+    def __init__(self, world, x,y):
+        super().__init__(world, x, y)
+
+    def update(self, delta):
+        pass
+class Stove7(Model):
+
+    def __init__(self, world, x,y):
+        super().__init__(world, x, y)
+
+    def update(self, delta):
+        pass
+class Stove8(Model):
+
+    def __init__(self, world, x,y):
+        super().__init__(world, x, y)
+
+    def update(self, delta):
+        pass
+class Stove9(Model):
+
+    def __init__(self, world, x,y):
+        super().__init__(world, x, y)
+
+    def update(self, delta):
+        pass
+class Stove10(Model):
+
+    def __init__(self, world, x,y):
+        super().__init__(world, x, y)
+
+    def update(self, delta):
+        pass
+
+class Button1(Model):
+
+    def __init__(self, world, x,y):
+        super().__init__(world, x, y)
+        self.x = x
+        self.y = y
+    def update(self, delta):
+        pass
+class Button2(Model):
+
+    def __init__(self, world, x,y):
+        super().__init__(world, x, y)
+        self.x = x
+        self.y = y
+    def update(self, delta):
+        pass
+class Button3(Model):
+
+    def __init__(self, world, x,y):
+        super().__init__(world, x, y)
+        self.x = x
+        self.y = y
+    def update(self, delta):
+        pass
 
 class World:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
         
-        self.player = Player(self,100,100)
-        self.beef = Beef(self,500,100)
-        self.pig = Pig(self,570,100)
-        self.chic = Chic(self,640,100)
-        self.tau = Tau(self,240,100)
-        self.plate = Plate(self,240,240)
-        self.beefW = BeefWell(self,65,240)
-        self.pigW = PigWell(self,100,240)
-        self.chicW = ChicWell(self,135,240) 
+        #create player and set position.
+        self.player = Player(self,500,500)
+        #make the stove by a piece of stove(10piece)
+        self.stove1 = Stove1(self,60,100) 
+        self.stove2 = Stove2(self,100,100) 
+        self.stove3 = Stove3(self,140,100) 
+        self.stove4 = Stove4(self,183,100) 
+        self.stove5 = Stove5(self,226,100) 
+        self.stove6 = Stove6(self,266,100) 
+        self.stove7 = Stove7(self,306,100) 
+        self.stove8 = Stove8(self,343,100) 
+        self.stove9 = Stove9(self,382,100) 
+        self.stove10 = Stove10(self,423,100)
+        #make buttons
+        self.button1 = Button1(self,545,100)
+        self.button2 = Button2(self,605,100)
+        self.button3 = Button3(self,665,100)
 
-        self.roast_time = roasting
-        self.timer_text = None
 
-    def on_mouse_drag(self,x, y, dx, dy, buttons, modifiers):
-        if self.player.hit(self.beef, 20):
-            if buttons and mouse.LEFT:
-                self.beef.x = x
-                self.beef.y = y
-        if self.player.hit(self.pig, 20):
-            if buttons and mouse.LEFT:
-                self.pig.x = x
-                self.pig.y = y
-        if self.player.hit(self.chic, 20):
-            if buttons and mouse.LEFT:
-                self.chic.x = x
-                self.chic.y = y
-
-    def draw_text(self):
-        minutes = int(self.roast_time) // 60
-        seconds = int(self.roast_time) % 60
-
-        output = f"Time: {minutes:02d}:{seconds:02d}"
-
-        if not self.timer_text or self.timer_text.text != output:
-            self.timer_text = arcade.create_text(output, arcade.color.BLACK, 20)
-
-        # Output the timer text.
-        arcade.render_text(self.timer_text, 500, 30)
-
-    def update(self, delta):
-
+    def update(self,delta):
         self.player.update(delta)
-        self.beef.update(delta)
-        self.pig.update(delta)
-        self.chic.update(delta)
-        self.tau.update(delta)
-        self.plate.update(delta)
-        self.beefW.update(delta)
-        self.pigW.update(delta)
-        self.chicW.update(delta)
+        self.stove1.update(delta)
+        self.stove2.update(delta)
+        self.stove3.update(delta)
+        self.stove4.update(delta)
+        self.stove5.update(delta)
+        self.stove6.update(delta)
+        self.stove7.update(delta)
+        self.stove8.update(delta)
+        self.stove9.update(delta)
+        self.stove10.update(delta)
+        self.button1.update(delta)
+        self.button2.update(delta)
+        self.button3.update(delta)
 
-        if self.beef.x>28 and self.beef.x<460 and self.beef.y>30 and self.beef.y<100:
-            print("Now, on the stove.")
-            self.roast_time -= delta
-            seconds = int(self.roast_time) % 60
-            minutes = int(self.roast_time) // 60
-            
-            if seconds==0 or minutes<0 :
-                print("Now, roasting")
-                self.beefW.x = self.beef.x 
-                self.beefW.y = self.beef.y
-                self.beef.x = 500 
-                self.beef.y = 100
-            
-        #if self.pig.x>28 and self.pig.x<460 and self.pig.y>30 and self.pig.y<100:
 
-        #if self.chic.x>28 and self.chic.x<460 and self.chic.y>30 and self.chic.y<100:
+        if self.player.hit(self.stove1, 20):
+            print("Touch stove1")
+        if self.player.hit(self.stove2, 20):
+            print("Touch stove2")
+        if self.player.hit(self.stove3, 20):
+            print("Touch stove3")
 
-        
+
+
+
--- BBQstore.py
+++ BBQstore.py
@@ -1,8 +1,9 @@
 import arcade
-from Actor import World,Player,Beef,Pig,Chic,Plate,Tau
+import random
+from Actor2 import World,Player,Stove1,Stove2,Stove3,Stove4,Stove5,Stove6,Stove7,Stove8,Stove9,Stove10,Model,Collectable,Beef,Pig,Chicken
 from pyglet.window import mouse
 
- 
+roasting = 5
 SCREEN_WIDTH = 760
 SCREEN_HEIGHT = 550
 
@@ -26,45 +27,154 @@ class BBQwindow(arcade.Window):
         super().__init__(width, height)
         arcade.set_background_color(arcade.color.PINK)
         self.world = World(width, height) 
-        self.Tau_sprite = ModelSprite("image/Tau.png",model=self.world.tau)   
-        self.Plate_sprite = ModelSprite("image/Plate.png",model=self.world.plate)
-        self.Beef_sprite = ModelSprite("image/BeefRare.png",model=self.world.beef)
-        self.Pig_sprite = ModelSprite("image/PigRare.png",model=self.world.pig)
-        self.Chic_sprite = ModelSprite("image/ChicRare.png",model=self.world.chic)
-        self.BeefW_sprite = ModelSprite("image/BeefWell.png",model=self.world.beefW)
-        self.PigW_sprite = ModelSprite("image/PigWell.png",model=self.world.pigW)
-        self.ChicW_sprite = ModelSprite("image/ChicWell.png",model=self.world.chicW)   
+        #initialize player
         self.player_sprite = ModelSprite("image/hand1.png",model=self.world.player)
 
+        #initialize stove
+        self.Stove1_sprite = ModelSprite("image/ApieceofStove/images/Stove_01.jpg",model=self.world.stove1) 
+        self.Stove2_sprite = ModelSprite("image/ApieceofStove/images/Stove_02.jpg",model=self.world.stove2)
+        self.Stove3_sprite = ModelSprite("image/ApieceofStove/images/Stove_03.jpg",model=self.world.stove3)
+        self.Stove4_sprite = ModelSprite("image/ApieceofStove/images/Stove_04.jpg",model=self.world.stove4)
+        self.Stove5_sprite = ModelSprite("image/ApieceofStove/images/Stove_05.jpg",model=self.world.stove5)
+        self.Stove6_sprite = ModelSprite("image/ApieceofStove/images/Stove_06.jpg",model=self.world.stove6)
+        self.Stove7_sprite = ModelSprite("image/ApieceofStove/images/Stove_07.jpg",model=self.world.stove7)
+        self.Stove8_sprite = ModelSprite("image/ApieceofStove/images/Stove_08.jpg",model=self.world.stove8)
+        self.Stove9_sprite = ModelSprite("image/ApieceofStove/images/Stove_09.jpg",model=self.world.stove9)
+        self.Stove10_sprite = ModelSprite("image/ApieceofStove/images/Stove_10.jpg",model=self.world.stove10) 
+        
+        #initialize button
+        self.Button1_sprite = ModelSprite("image/Button1.png",model=self.world.button1)
+        self.Button2_sprite = ModelSprite("image/Button2.png",model=self.world.button2)
+        self.Button3_sprite = ModelSprite("image/Button3.png",model=self.world.button3)
+
+        #initialize BBQ
+        self.Beef_list = arcade.SpriteList()
+        self.BeefW_list = arcade.SpriteList()
+        self.Pig_list = arcade.SpriteList()
+        self.PigW_list = arcade.SpriteList()
+        self.Chic_list = arcade.SpriteList()
+        self.ChicW_list = arcade.SpriteList()
+        
+        #initialize_time_roasting
+        self.roast_time = roasting
 
     def on_mouse_motion(self, x, y, dx, dy):
         self.world.player.on_mouse_motion(x, y, dx, dy)
 
-    #def on_mouse_press(x, y, button, modifiers):
-        #self.world.on_mouse_drag(x, y, buttons, modifiers)
 
-    def on_mouse_drag(self,x, y, dx, dy, buttons, modifiers):
-        self.world.on_mouse_drag(x, y, dx, dy, buttons, modifiers)
-        self.world.player.on_mouse_drag(x, y, dx, dy, buttons, modifiers)
+    #def on_mouse_drag(self,x, y, dx, dy, buttons, modifiers):
+        #self.world.on_mouse_drag(x, y, dx, dy, buttons, modifiers)
+
+    def on_mouse_press(self,x,y,buttons,modifiers):
+        #Check player hit button
+        if self.world.player.hit(self.world.button2, 20):
+            print("press button2.")
+            pig = Pig("image/PigRare.png", 1)
+            pig.center_x = self.world.stove2.x
+            pig.center_y = self.world.stove2.y
+            # Add the Pig to the appropriate lists
+            self.Pig_list.append(pig)
+            print("Pig iss on stove")
+
+        elif self.world.player.hit(self.world.button1, 20):
+            print("press button1.")
+            beef = Beef("image/BeefRare.png", 1)
+            beef.center_x = self.world.stove1.x
+            beef.center_y = self.world.stove1.y
+            # Add the Beef to the appropriate lists
+            self.Beef_list.append(beef)
+
+        elif self.world.player.hit(self.world.button3, 20):
+            print("press button3.")
+            chic = Chicken("image/ChicRare.png", 1)
+            chic.center_x = self.world.stove3.x
+            chic.center_y = self.world.stove3.y
+            # Add the Chic to the appropriate lists
+            self.Chic_list.append(chic)
 
     def on_draw(self):
         arcade.start_render()  
-        
-        self.Tau_sprite.draw()
-        self.Plate_sprite.draw()
-        self.Beef_sprite.draw()
-        self.Pig_sprite.draw()
-        self.Chic_sprite.draw()
-        self.BeefW_sprite.draw()
-        self.PigW_sprite.draw()
-        self.ChicW_sprite.draw()
+        #draw all a piece of stove.
+        self.Stove1_sprite.draw()
+        self.Stove2_sprite.draw()
+        self.Stove3_sprite.draw()
+        self.Stove4_sprite.draw()
+        self.Stove5_sprite.draw()
+        self.Stove6_sprite.draw()
+        self.Stove7_sprite.draw()
+        self.Stove8_sprite.draw()
+        self.Stove9_sprite.draw()
+        self.Stove10_sprite.draw()
+
+        #draw all Button
+        self.Button1_sprite.draw()
+        self.Button2_sprite.draw()
+        self.Button3_sprite.draw()
+
+        #draw all BBQ
+        self.Beef_list.draw()
+
+        #draw player
         self.player_sprite.draw()
-    
-        self.world.draw_text()
+        
 
     def update(self,delta):
         
         self.world.update(delta)
+
+        stove2_status = \
+            arcade.check_for_collision_with_list(self.Stove2_sprite,
+                                                 self.Pig_list)
+        for pig in stove2_status:    
+            # Position of Beef
+            pig.center_x = self.world.stove2.x
+            pig.center_y = self.world.stove2.y
+            #Swap beefrare to beef well.
+            self.roast_time -= delta
+            seconds = int(self.roast_time) % 60
+            minutes = int(self.roast_time) // 60
+            if seconds==0 or minutes<0 :
+                pig.texture = arcade.load_texture("image/PigWell.png")
+                pig.changed = True
+                # Add the Pig to the appropriate lists
+                self.PigW_list.append(pig)
+
+
+        stove1_status = \
+            arcade.check_for_collision_with_list(self.Stove1_sprite,
+                                                 self.Beef_list)
+        for beef in stove1_status:    
+            # Position of Beef
+            beef.center_x = self.world.stove1.x
+            beef.center_y = self.world.stove1.y
+            #Swap beef rare to beef well.
+            self.roast_time -= delta
+            seconds = int(self.roast_time) % 60
+            minutes = int(self.roast_time) // 60
+            if seconds==0 or minutes<0 :
+                beef.texture = arcade.load_texture("image/BeefWell.png")
+                beef.changed = True
+                # Add the Beef to the appropriate lists
+                self.BeefW_list.append(beef)
+
+        
+        stove3_status = \
+            arcade.check_for_collision_with_list(self.Stove3_sprite,
+                                                 self.Chic_list)
+        for chic in stove3_status:    
+            # Position of Beef
+            chic.center_x = self.world.stove3.x
+            chic.center_y = self.world.stove3.y
+            #Swap beefrare to beef well.
+            self.roast_time -= delta
+            seconds = int(self.roast_time) % 60
+            minutes = int(self.roast_time) // 60
+            if seconds==0 or minutes<0 :
+                chic.texture = arcade.load_texture("image/ChicWell.png")
+                chic.changed = True
+                # Add the Chic to the appropriate lists
+                self.ChicW_list.append(chic)
+
 
  
 if __name__ == '__main__':
