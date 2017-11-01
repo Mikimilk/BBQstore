b50cc2b3856d5ce737d6dbf36d4ce2a3db12308b
Ravin <milknaiti@hotmail.com>
2017-11-01 15:27:48 +0700

    Add New game page, how to play and end game page.

--- BBQstore.py
+++ BBQstore.py
@@ -1,11 +1,13 @@
 import arcade
 from random import randint
-from Actor2 import World,Player,Stove1,Stove2,Stove3,Stove4,Stove5,Stove6,Stove7,Stove8,Stove9,Stove10,Model,Collectable,Beef,Pig,Chicken,Bag,Order
+from Actor import World,Player,Stove1,Stove2,Stove3,Stove4,Stove5,Stove6,Stove7,Stove8,Stove9,Stove10,Model,Collectable,Beef,Pig,Chicken,Bag,Order
 from pyglet.window import mouse
+from pyglet.window import key
 
 roasting = 5
 SCREEN_WIDTH = 760
 SCREEN_HEIGHT = 550
+GAMETIME =59.0
 
 class ModelSprite(arcade.Sprite):
     def __init__(self, *args, **kwargs):
@@ -26,7 +28,6 @@ class BBQwindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
         self.world = World(width, height)
-        self.background = arcade.load_texture("image/BGG.png") 
         #initialize player
         self.player_sprite = ModelSprite("image/hand1.png",model=self.world.player)
 
@@ -51,28 +52,62 @@ class BBQwindow(arcade.Window):
         #initialize Bin
         self.Bin_sprite = ModelSprite("image/Bin.png",model=self.world.bin)
         #make Game Time
-        self.Game_Time = 60.0
+        self.Game_Time = GAMETIME
         self.timer_text = None
         #make text position in each orders.
         self.Order_text_position_x = [[50,125,80],[345,420,370],[150,225,180]]
         self.Order_text_position_y = [[460,460,430],[430,430,400],[330,330,300]]
-        
 
+        #initialize mode game
+        self.GAME_OVER = False
+        self.NEW_GAME = True
+        New_Game = arcade.load_texture("image/New_Game.png")
+        Game_Play = arcade.load_texture("image/BGG.png") 
+        How_to_play1 = arcade.load_texture("image/How_to_play1.png")
+        How_to_play2 = arcade.load_texture("image/How_to_play2.png")
+        Game_Over = arcade.load_texture("image/Game_Over.png")
+        self.instructions = []
+
+        self.instructions.append(New_Game)
+        self.instructions.append(How_to_play1)
+        self.instructions.append(How_to_play2)
+        self.instructions.append(Game_Play)
+        self.instructions.append(Game_Over)
+
+        self.current_state = 0
+        
     def on_mouse_motion(self, x, y, dx, dy):
         self.world.player.on_mouse_motion(x, y, dx, dy)
-
-
-    #def on_mouse_drag(self,x, y, dx, dy, buttons, modifiers):
-        #self.world.player.on_mouse_drag(x, y, dx, dy, buttons, modifiers)
 
     def on_mouse_press(self,x,y,buttons,modifiers):
         #Check player hit button
         self.world.on_mouse_press(x,y,buttons,modifiers)
 
-    def on_draw(self):
-        arcade.start_render()  
-        arcade.draw_texture_rectangle(SCREEN_WIDTH//2 , SCREEN_HEIGHT//2 ,
-                                      SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+    def draw_instructions_page(self, page_number):
+        #Draw an instruction page. Load the page as an image.
+
+        page_texture = self.instructions[page_number]
+        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
+                                      page_texture.width,
+                                      page_texture.height, page_texture, 0)
+
+    def on_key_press(self, key, key_modifiers):
+
+        if self.current_state == 0 :
+            self.current_state = 1
+
+        elif self.current_state == 1 :
+            self.current_state = 2 
+
+        elif self.current_state == 2 :
+            self.current_state = 3
+
+        elif self.current_state == 4 :
+            self.current_state = 0   
+        print(self.current_state)
+
+    def draw_game(self):
+
         #draw bag
         self.Bag_sprite.draw()
 
@@ -151,13 +186,36 @@ class BBQwindow(arcade.Window):
 
         #draw player
         self.player_sprite.draw()
+
+    def on_draw(self): 
+        arcade.start_render()
+
+        if self.current_state == 0 :
+            self.draw_instructions_page(0)
+        elif self.current_state == 1 :
+            self.draw_instructions_page(1)
+        elif self.current_state == 2 :
+            self.draw_instructions_page(2)
+        elif self.current_state == 3 :
+            self.draw_instructions_page(3)
+            self.draw_game()
+        elif self.current_state == 4 :
+            self.draw_instructions_page(4)
+
         
 
     def update(self,delta):
         
-        self.world.update(delta)
-        self.Game_Time -= delta
- 
+        seconds = int(self.Game_Time) % 60
+        minutes = int(self.Game_Time) // 60
+
+        if self.current_state == 3:
+            self.world.update(delta)
+            self.Game_Time -= delta
+            if minutes != 0:
+                self.current_state = 4
+
+
 if __name__ == '__main__':
     window = BBQwindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.run()
